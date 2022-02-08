b5d5d9067f4ba8577a1655f4d3d6478d3a314b42
Sworddafence <90822210+Sworddafence@users.noreply.github.com>
2022-02-08 08:13:18 -0800

    Update rice.sh

--- rice.sh
+++ rice.sh
@@ -5,8 +5,8 @@ git clone https://github.com/EliverLara/candy-icons.git
 cd ..
 mkdir -p ~/.themes
 cd .themes
-git clone https://github.com/EliverLara/Sweet.git
+git clone https://github.com/EliverLara/Nordic.git
 cd ..
-gsettings set org.gnome.desktop.interface gtk-theme "Sweet"
-gsettings set org.gnome.desktop.wm.preferences theme "Sweet"
+gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
+gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
 gsettings set org.gnome.desktop.interface icon-theme candy-icons
