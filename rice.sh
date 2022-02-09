a01e1d1a3f9df1db21e3e4fd5a2571b93edf63b6
Sworddafence <90822210+Sworddafence@users.noreply.github.com>
2022-02-08 23:30:21 -0800

    Update rice.sh

--- rice.sh
+++ rice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget --output-document=rice.sh https://raw.githubusercontent.com/Sworddafence/JustinOS/main/rice.sh
-
+apt-get -y install git
+wget --output-document=back.jpg https://www.fanbolt.com/storage/2021/08/anime-iphone-wallpaper.jpg
 gsettings set org.gnome.desktop.background picture-uri file:////home/john/back.jpg
 mkdir /home/john/.icons
 git clone https://github.com/EliverLara/candy-icons.git ./.icons/candy-icons
