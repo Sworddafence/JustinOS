f7085cb3930bea9c679e50028be84ef9dde2a1eb
Sworddafence <90822210+Sworddafence@users.noreply.github.com>
2022-02-08 22:33:21 -0800

    Update JustinOS.sh

--- JustinOS.sh
+++ JustinOS.sh
@@ -6,6 +6,18 @@ apt-get -y install python3 python3-dev python3-pip git
 pip3 install --upgrade git+https://github.com/arthaud/python3-pwntools.git
 git clone https://github.com/longld/peda.git ~/peda
 echo "source ~/peda/peda.py" >> ~/.gdbinit
+
 wget --output-document=back.jpg https://www.fanbolt.com/storage/2021/08/anime-iphone-wallpaper.jpg
-wget --output-document=rice.sh https://raw.githubusercontent.com/Sworddafence/JustinOS/main/rice.sh
+gsettings set org.gnome.desktop.background picture-uri file:////home/john/back.jpg
+mkdir -p ~/.icons
+cd .icons
+git clone https://github.com/EliverLara/candy-icons.git
+cd ..
+mkdir -p ~/.themes
+cd .themes
+git clone https://github.com/EliverLara/Nordic.git
+cd ..
+gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
+gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
+gsettings set org.gnome.desktop.interface icon-theme candy-icons
 
