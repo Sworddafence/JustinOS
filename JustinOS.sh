b6ac8d5627bce5ff28fc6f51f1a292c952c7a5ac
Sworddafence <90822210+Sworddafence@users.noreply.github.com>
2022-02-08 23:01:07 -0800

    Update JustinOS.sh

--- JustinOS.sh
+++ JustinOS.sh
@@ -11,13 +11,9 @@ wget --output-document=rice.sh https://raw.githubusercontent.com/Sworddafence/Ju
 
 gsettings set org.gnome.desktop.background picture-uri file:////home/john/back.jpg
 mkdir /home/john/.icons
-cd .icons
-git clone https://github.com/EliverLara/candy-icons.git
-cd ..
+git clone https://github.com/EliverLara/candy-icons.git ./.icons/candy-icons
 mkdir /home/john/.themes
-cd .themes
-git clone https://github.com/EliverLara/Nordic.git
-cd ..
+git clone https://github.com/EliverLara/Nordic.git ./.themes/Nordic
 gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
 gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
 gsettings set org.gnome.desktop.interface icon-theme candy-icons
