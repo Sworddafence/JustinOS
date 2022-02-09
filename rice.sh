e8675fbc017810e00afb4b4667df40eb5f5d5be1
Sworddafence <90822210+Sworddafence@users.noreply.github.com>
2022-02-08 23:22:56 -0800

    Update rice.sh

--- rice.sh
+++ rice.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+wget --output-document=rice.sh https://raw.githubusercontent.com/Sworddafence/JustinOS/main/rice.sh
+
 gsettings set org.gnome.desktop.background picture-uri file:////home/john/back.jpg
-mkdir -p ~/.icons
-cd .icons
-git clone https://github.com/EliverLara/candy-icons.git
-cd ..
-mkdir -p ~/.themes
-cd .themes
-git clone https://github.com/EliverLara/Nordic.git
-cd ..
+mkdir /home/john/.icons
+git clone https://github.com/EliverLara/candy-icons.git ./.icons/candy-icons
+mkdir /home/john/.themes
+git clone https://github.com/EliverLara/Nordic.git ./.themes/Nordic
 gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
 gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
 gsettings set org.gnome.desktop.interface icon-theme candy-icons
